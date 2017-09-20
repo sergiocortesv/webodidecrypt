1434b80a2e7539cf5f257aeded83632b7c7ec5d4
Sergio Armando Cortes Valladolid <scortes@baufest.com>
2017-09-20 16:24:24 -0500

    fix : odiDecryptPage not showing model updates - removed href=# attribute on anchor element

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,20 +1,18 @@
 var myApp = angular.module('myApp',['ngRoute','ngFileUpload']);
 
 myApp.controller('decryptController',['$scope','$http',function($scope,$http){
-	$scope.encryptedStr = '';
-	$scope.decryptedStr = '';
-    $scope.errorMsg = null;
-
+	
 	$scope.decrypt = function(){
-        $scope.decryptedStr = '';
-        $scope.errorMsg = null;
+		$scope.decryptedStr = '';
+		$scope.errorMsg = null;
+		
 		$http.post('odidecrypt', $scope.encryptedStr)
 		.then(function successCallback(response) {
 			$scope.decryptedStr = response.data.decryptedStr;
 		}, function errorCallback(response) {
 			$scope.errorMsg = response.data.error;
 		});
-	}
+	};
 }]);
 
 myApp.controller('odiXmlDecryptController',['$scope','$http','Upload',function($scope,$http,Upload){
